2222eb531a576ab4f2d4532aef4bbb0ed6a559e8
walaawajeeh4 <walaawajeeh06.gmail.com>
2025-07-08 22:11:45 +0300

    new

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-document.getElementById("demo").i
-nnerhtml = "hello js";
+var h1text = document.getElementById('heading1')
+h1text.innerText ="changed using js right now!"
+function changeColor(){
+    var colortext = document.getElementById('pcolor').style.color = 'red';
+}
